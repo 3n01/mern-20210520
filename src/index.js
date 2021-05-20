aff04f7367f353e72b48505a8ed39be0fde9c015
Enol Cachero Ramírez <enol@23xy.enol.com>
2021-05-20 10:50:18 +0200

    Configuracion basica express

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const morgan = require('morgan');
 
-app.listen(port , () => {
-    console.log(`Escuchando por puerto: ${port}`);
+//settings
+app.set('port', process.env.PORT || port);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());//sustituye a BodyParser
+
+//routes
+app.use(require('./routes/test.routes'));
+
+//static files
+
+
+//start
+app.listen(app.get('port') , () => {
+    console.log(`Escuchando por puerto: ${app.get('port')}`);
 })
